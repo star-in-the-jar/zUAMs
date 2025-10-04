9329e708a27e301a39ca269d6810363f3edf29ad
Władysław Kuczerenko <w.kuczerenko@crehler.com>
2025-10-04 16:15:13 +0200

    Implement home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = () => {
-  return <h2>Home Page</h2>;
+  const navigate = useNavigate();
+  const [pension, setPension] = useState<string>("");
+  const [lastValid, setLastValid] = useState<string>("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+    if (val === "") {
+      setPension("");
+      setLastValid("");
+      return;
+    }
+    if (isNaN(Number(val))) {
+      setPension(lastValid);
+    } else {
+      setPension(val);
+      setLastValid(val);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    if (pension === "" || isNaN(Number(pension)) || Number(pension) <= 0) {
+      return;
+    }
+    e.preventDefault();
+    navigate("/result");
+  };
+
+  return (
+    <section className="flex flex-col items-center justify-center min-h-[70vh] gap-8">
+      <h1 className="text-4xl font-bold text-center mb-6">
+        Jaka chcesz mieć emeryturę?
+      </h1>
+      <form onSubmit={handleSubmit} className="flex gap-x-1 items-center">
+        <label className="input input-lg">
+          <input
+            value={pension}
+            className="grow"
+            placeholder="3000"
+            onInput={handleInput}
+          />
+          <span>PLN</span>
+        </label>
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className="btn btn-primary btn-lg"
+        >
+          Sprawdź
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Home;

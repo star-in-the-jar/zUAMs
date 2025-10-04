83c5961e4c7a67dd464cc74f10194d79fe8efd20
szymonjozwiakzip <jozwiakszymon03@gmail.com>
2025-10-04 20:29:26 +0200

    WIP future planning

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/views/Home";
 import Calculator from "@/views/Calculator";
+import FuturePlanning from "@/views/FuturePlanning";
 import Layout from "@/components/Layout";
 import LogoZeus from "@/components/LogoZeus";
 
 function App() {
   return (
+    <div className="px-32 py-16 min-h-screen flex flex-col">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/future-planning" element={<FuturePlanning />} />
+        </Routes>
+      </BrowserRouter>
     <div className="px-32 py-16 min-h-screen">
       <header className="flex items-center mb-16">
         <LogoZeus />

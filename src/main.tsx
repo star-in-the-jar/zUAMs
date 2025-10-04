8e578a69dbd37d159bcf05a501954f3ffb58fe29
Stanisław Jarocki <s.jarocki354@gmail.com>
2025-10-04 20:31:57 +0200

    Remove redundant component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ZusChatbot from "./components/ZusChatbot";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ZusChatbot />
     <App />
   </StrictMode>
 );
